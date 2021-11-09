898ec8f6cb6159463777cef2a702fce213fca615
raninder <raninderdhillon@gmail.com>
2021-11-09 02:48:26 +0000

    tinyapp

---
+++ express_server2.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const PORT = 8080;
+
+const app = express();
+
+const urlDatabase = {
+    "b2xVn2": "http://www.lighthouselabs.ca",
+    "9sm5xK": "http://www.google.com"
+  };
+
+app.get('/',(req, res) => {
+res.send("Hello");
+})
+app.get("/urls.json", (req, res) => {
+    res.json(urlDatabase);
+  });
+  app.get("/hello", (req, res) => {
+    res.send("<html><body>Hello <b>World</b></body></html>\n");
+  });
+  app.get("/set", (req, res) => {
+    const a = 1;
+    res.send(`a = ${a}`);
+   });
+   app.get("/fetch", (req, res) => {
+    res.send(`a = ${a}`);
+   });
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}!`);
+  });
