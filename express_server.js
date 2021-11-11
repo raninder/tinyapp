5d3edbcfca1fd9d85f4fcb1519e457b707d4ba1d
raninder <raninderdhillon@gmail.com>
2021-11-11 12:33:13 +0000

    tinyapp login register

--- express_server.js
+++ express_server.js
@@ -13,15 +13,31 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+const users = {
+    "userRandomID": {
+        id: "userRandomID",
+        email: "user1@example.com",
+        password: "user1"
+    },
+    "user2RandomID": {
+        id: "user2RandomID",
+        email: "user2@example.com",
+        password: "user2"
+    }
+}
+
 //render form to user for entering url
 app.get("/urls/new", (req, res) => {
     const uid = req.cookies["user_id"];
-
+//check if cookies id match with users database id
     for (let k in users) {
         let key = users[k];
         if (key.id === uid) {
             //userData = {id:key.id, email:key.email, password:key.password};
             userEmailId = key.email;
+        }
+        else {
+            userEmailId = null;
         }
     }
     const templateVars = {
@@ -46,17 +62,21 @@ app.post("/urls", (req, res) => {
 //renders all short and long urls stored in url database
 app.get("/urls", (req, res) => {
     const uid = req.cookies["user_id"];
-    console.log(uid);
-    //let userData= {};
+    //if there are no cookies,email should be null
+    userEmailId = null;
+   
     for (let k in users) {
         let key = users[k];
+        //if cookies id match with any users id
         if (key.id === uid) {
             //userData = {id:key.id, email:key.email, password:key.password};
             userEmailId = key.email;
+            //console.log("userEmailId within loop", userEmailId);
         }
+
     }
     const templateVars = {
-        user_id: req.cookies["user_id"],
+        user_id: uid,
         urls: urlDatabase,
         useremail: userEmailId
     };
@@ -66,22 +86,23 @@ app.get("/urls", (req, res) => {
 //renders shortURL for a given url
 app.get("/urls/:shortURL", (req, res) => {
     const uid = req.cookies["user_id"];
-    console.log(uid);
-    //let userData= {};
+   
     for (let k in users) {
         let key = users[k];
         if (key.id === uid) {
             //userData = {id:key.id, email:key.email, password:key.password};
             userEmailId = key.email;
+        } else {
+            userEmailId = null;
         }
     }
-    //console.log("edit:", req.body);
-    const templateVars = { 
-        user_id: req.cookies["user_id"], 
-        shortURL: req.params.shortURL, 
+    
+    const templateVars = {
+        user_id: req.cookies["user_id"],
+        shortURL: req.params.shortURL,
         longURL: urlDatabase[req.params.shortURL],
         useremail: userEmailId
-     };
+    };
     res.render("urls_show", templateVars);
 
 });
@@ -89,7 +110,7 @@ app.get("/urls/:shortURL", (req, res) => {
 //redirect user to lorngURL when shorURL clicked
 app.get("/u/:shortURL", (req, res) => {
     const longURL = urlDatabase[req.params.shortURL];
-    res.status(300).redirect("https://" + longURL);
+    res.redirect("https://" + longURL);
 });
 
 //delete shortURLs
@@ -106,70 +127,55 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 
 //update url
 app.post("/urls/:shortURL", (req, res) => {
-    //const id = req.params.id;
     const newURL = req.body.newURL;
     const shortURL = req.params.shortURL;
     urlDatabase[shortURL] = newURL;
     res.redirect('/urls');
-    //console.log(req.body);
 });
 
 app.post("/login", (req, res) => {
-    //const cookie = req.body.username;
     const email = req.body.email;
     const password = req.body.password;
-
-    //res.cookie('username', cookie);
-    res.cookie('user_id', id);
-
+    //password and email not empty
     if (password && email) {
         for (let k in users) {
             let key = users[k];
-            if (key.email !== email) {
-                //userData = {id:key.id, email:key.email, password:key.password};
-                res.send({ error: '403 Email doesnt exists' });
-            }
-            else {
-                if (key.password === password) {
-                    res.cookie('user_id', key.id);
-                    res.redirect('/urls');
-                }
-                else {
-                    res.send({ error: '403 Password doesnt match' });
-                }
+        
+            if (key.email === email && key.password === password) {
+                res.cookie('user_id', key.id);
+                console.log("login user-id", key.id)
+                res.redirect('/urls');
             }
         }
+        if (key.email !== email) {
+            res.send({ error: '403 Email doesnt exists' });
+        }
+        else {
+            res.send({ error: '403 Password doesnt match' });
+        }
     }
-
+    
+    else {
+        res.send({ error: "Enter email and password" });
+    }
 })
 
-
+// logout
 app.post("/logout", (req, res) => {
     res.clearCookie("user_id");
     res.redirect('/urls');
 });
 
-
-const users = {
-    "userRandomID": {
-        id: "userRandomID",
-        email: "user@example.com",
-        password: "purple-monkey-dinosaur"
-    },
-    "user2RandomID": {
-        id: "user2RandomID",
-        email: "user2@example.com",
-        password: "dishwasher-funk"
-    }
-}
+//render registration form
 app.get("/register", (req, res) => {
-
+    //pass useremail as null to partial/_headers 
     const templateVars = {
         useremail: null
     }
-    res.render("register", templateVars);
+    res.render("user_register", templateVars);
 
 });
+
 app.post("/register", (req, res) => {
 
     const id = generateRandomString();
@@ -180,24 +186,21 @@ app.post("/register", (req, res) => {
         for (let k in users) {
             let key = users[k];
             if (key.email === email) {
-                //userData = {id:key.id, email:key.email, password:key.password};
-                res.status(400).send({ error: '404 Email already exists' });
-                
+                res.send({ error: '404 Email already exists' });
             }
         }
+        //create new user with key-values & add to database
         const newUser = { id, email, password };
         users.user3RandomId = newUser;
     }
     else {
-        res.send({ error: '404 Not found' });
-        //res.status(400).redirect("/register");
+        res.send({ error: '404 enter email and password' });
     }
-    //console.log(users);
-    res.cookie('user_id', id);
-    res.status(200).redirect("/urls/");
+    res.redirect("/urls/");
 
 });
 
+//render login form
 app.get("/login", (req, res) => {
     const templateVars = {
         useremail: null
@@ -209,6 +212,7 @@ app.get("/login", (req, res) => {
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
 });
+
 //generate 6 letter random string to use as shortURL
 function generateRandomString() {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
