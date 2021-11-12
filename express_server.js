b15f0734672525f1975cb3038c4261f1bd5f7456
raninder <raninderdhillon@gmail.com>
2021-11-12 22:48:49 +0000

    Final tinyapp

--- express_server.js
+++ express_server.js
@@ -37,6 +37,7 @@ app.get("/urls/new", (req, res) => {
         user_id: userID,
         useremail: userEmailId
     };
+
     //check if user logged in
     if (userEmailId) {
         res.render("urls_new", templateVars);
@@ -84,6 +85,8 @@ app.get("/urls", (req, res) => {
         urls: urlUser,
         useremail: userEmailId
     };
+    console.log("templatevars",templateVars);
+
     //user only displaying urls for own
     if (urlUser) {
         res.render("urls_index", templateVars);
@@ -123,6 +126,8 @@ app.get("/u/:shortURL", (req, res) => {
     const shortURL = req.params.shortURL;
     const longURL = urlDatabase[req.params.shortURL].longURL;
     const reg = new RegExp('([a-zA-Z\d]+://)?((\w+:\w+@)?([a-zA-Z\d.-]+\.[A-Za-z]{2,4})(:\d+)?(/.*)?)', 'i')
+    
+    //check if url is valid
     if (reg.test(longURL)) {
         res.redirect(`https://${longURL}`);
     }
@@ -205,81 +210,62 @@ app.post('/login', (req, res) => {
         return res.status(400).send('no user with that email found')
     }
 
-    // found the user, now does their password match?
+    // found the user, check if passwords match?
     bcrypt.compare(password, user.password, (err, success) => {
         if (!success) {
             return res.status(400).send('password does not match')
         }
-
+      
         // telling the browswer to set this cookie
-
-        req.session.userId = user.id;
-
+        req.session.user_id = user.id;
         res.redirect('/urls');
     })
 });
-    // app.post("/login", (req, res) => {
-    //     const email = req.body.email;
-    //     const password = req.body.password;
-    //     const user = getUserByEmail(email, users);
-    //     let uid = users[user].id;
-    //     req.session.user_id = uid;
-    //     if (user) {
-    //         if (users[user].password === password) {
-    //             // const success = bcrypt.compareSync(password, users[user].password);
-    //             // if (!success) {
-    //             res.send(' Password  match');
-    //         }
-    //         //  req.session.user_id = users[user].id;
-    //         console.log("user-id", users[user].id);
-    //         res.redirect('/urls');
-    //     }
-    //     else {
-    //         res.send({ error: 'user doesnt exist' });
-    //     }
-    // })
 
-    // // logout
-    // app.post("/logout", (req, res) => {
-    //     req.session = null;
-    //     res.redirect('/urls');
-    // });
 
-    // display registration form
-    app.get("/register", (req, res) => {
-        const userID = req.session.user_id;
-        const templateVars = {
-            useremail: null //pass useremail as null to partial/_headers 
-        }
-        if (userID) {
-            res.redirect('/urls');
-        }
-        else{
-        res.render("user_register", templateVars);
-        }
-    });
+// logout
+app.post("/logout", (req, res) => {
+    //delete cookies
+    req.session = null;
+    res.redirect('/urls');
+});
 
-    // add new user in users database
-    app.post("/register", (req, res) => {
-        const id = generateRandomString();
-        const email = req.body.email;
-        const user = getUserByEmail(email, users);
-        const password = req.body.password;
-        const hashed = bcrypt.hashSync(password, 10);
-
-        if (!email || !password) {
-            return res.status(400).send('email and password cannot be blank');
-        }
-        if (user) {
-            return res.status(400).send({ error: '400 Email already exists' });
-        }
-        const newUser = { id, email, password: hashed };
-        users[id] = newUser;
+//display registration form
+app.get("/register", (req, res) => {
+    const userID = req.session.user_id;
+    const templateVars = {
+        useremail: null //pass useremail as null to partial/_headers 
+    }
+    if (userID) {
         res.redirect('/urls');
-    });
+    }
+    else {
+        res.render("user_register", templateVars);
+    }
+});
+
+// add new user to users database
+app.post("/register", (req, res) => {
+    const id = generateRandomString();
+    const email = req.body.email;
+    const user = getUserByEmail(email, users);
+    const password = req.body.password;
+    const hashed = bcrypt.hashSync(password, 10);
+
+    if (!email || !password) {
+        return res.status(400).send('email and password cannot be blank');
+    }
+    if (user) {
+        return res.status(400).send({ error: '400 Email already exists' });
+    }
+    const newUser = { id, email, password: hashed };
+    console.log("newuser", newUser);
+    users[id] = newUser;
+    res.redirect('/urls');
+});
 
 
-    app.listen(PORT, () => {
-        console.log(`Example app listening on port ${PORT}!`);
-    });
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}!`);
+});
 
